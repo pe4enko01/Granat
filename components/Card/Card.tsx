9c9f5d64094711ee5c5eebc1f73921cb61d6df59
pe4enko01 <pe4enko02@yandex.ru>
2022-09-08 17:22:48 +0300

    add fotter and card

---
+++ components/Card/Card.tsx
@@ -0,0 +1,12 @@
+import * as React from 'react';
+
+interface CardProps {
+}
+
+const Card: React.FunctionComponent<CardProps> = (props) => {
+  return (
+    
+  );
+};
+
+export default Card;
--- pages/index.tsx
+++ pages/index.tsx
@@ -5,11 +5,15 @@ import Head from 'next/head'
 import Image from 'next/image'
 import styles from '../styles/Home.module.css'
 import Header from '../components/Header/Header'
+import Footer from '../components/Footer/Fotter'
 
 const Home: NextPage = () => {
   return (
     <Provider store={store}>  
+
       <Header/>
+
+      <Footer/>
     </Provider>
      
   )
